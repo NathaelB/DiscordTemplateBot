57862e944ed16b9e27abc850b4ed71836f768ce0
Nathael Bonnal <kirua48000@gmail.com>
2021-11-05 17:48:11 +0100

    update

---
+++ src/settings.ts
@@ -0,0 +1,13 @@
+export enum Roles {
+    ADMIN = '892681156972122163',
+    SUPPORT = '905501319614300160'
+}
+
+export enum Channels {
+    TICKET = '894945893554483271'
+}
+
+export enum Category {
+    TICKET = '869993604297416704'
+}
+
