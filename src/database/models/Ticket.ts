fd624765ed462cf4297b4e28fea909d4060a9294
Nathael Bonnal <kirua48000@gmail.com>
2021-11-05 17:44:57 +0100

    :construction: create model

--- src/database/models/Ticket.ts
+++ src/database/models/Ticket.ts
@@ -1,8 +1,12 @@
 import { Model, BaseModel, Uuid } from '@discord-factory/storage-next'
 
-@Model('tickets')
+@Model('ticket')
 export default class Ticket extends BaseModel<Ticket> {
   public id: string
+
+  public ticket_id: string
+
+  public user_id: string
 
   public static beforeInsert (model: Ticket) {
     model.id = Uuid.generateV4()
--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -1,12 +1,16 @@
 import { Model, BaseModel, Uuid } from '@discord-factory/storage-next'
 
-@Model('users')
+@Model('user')
 export default class User extends BaseModel<User> {
   public id: string
 
   public username: string
 
   public user_id: string
+
+  public level: number
+
+  public exp: number
 
   public static beforeInsert (model: User) {
     model.id = Uuid.generateV4()
